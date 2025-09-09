2f71304cf63d8d0f9e740756dcd6862d3b20f59f
haenmanasaresgpxx1698 <haenmanasaresgpxx1698@gmail.com>
2025-09-09 23:10:09 +0800

    Update copyright year to 2025

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ function App() {
             </div>
           </div>
           <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-300">
-            <p>&copy; 2024 <span className="text-pink-400">PerfumePeony</span>. All rights reserved.</p>
+            <p>&copy; 2025 <span className="text-pink-400">PerfumePeony</span>. All rights reserved.</p>
           </div>
         </div>
       </footer>
